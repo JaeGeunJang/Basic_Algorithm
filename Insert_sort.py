c741aa20b9a0e9dc43e50663d95f14ea818a1993
JG <jaekeun950@gmail.com>
2023-01-01 03:54:07 +0900

    Insertion sort
    Implement Insertion Sort

---
+++ Insert_sort.py
@@ -0,0 +1,21 @@
+#Insrt Sort
+def Insert_sort(arr, acse = True) :
+    for i in range (1, len(arr)) :
+        key = arr[i]
+        j = i - 1
+        while j >= 0 :
+            if acse and arr[j] < key :
+                break
+            if not acse and arr[j] > key :
+                break
+            print(key, arr[j])
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
+
+#Test code
+import random
+arr = [random.randint(1, 100) for _ in range(10)]
+print(arr)
+print(Insert_sort(arr, False))
